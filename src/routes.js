9b5d079722ef0606cf58f22bf9d4c7e5efd10f6c
jorgehernandezch <jorge@galpha.co>
2022-10-26 11:33:11 -0300

    Add Profile Page

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
-export default function Routes(){
- return (
-   <BrowserRouter>
-     <Switch>
-       <Route path="/" exact component={Login}></Route>
-       <Route path="/dashboard" exact component={Dashboard}></Route>
-     </Switch>
-   </BrowserRouter>
- );
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login}></Route>
+        <Route path="/dashboard" exact component={Dashboard}></Route>
+        <Route path="/profile" exact component={Profile}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
